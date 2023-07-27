09b83916cbcb07e81adf5516fbf5fe67ff675448
Javier Valero Cejudo <javalce29@gmail.com>
2023-07-27 15:02:35 +0200

    Remove npm global packages installation

--- .config/yadm/bootstrap.d/03-install-node.sh
+++ .config/yadm/bootstrap.d/03-install-node.sh
@@ -1,8 +1,6 @@
 #!/bin/sh
 
-fnm_dir="$HOME/.local/share/fnm"
-fnm="$fnm_dir/fnm"
-npm="$fnm_dir/aliases/default/bin/npm"
+fnm="$HOME/.local/share/fnm/fnm"
 
 # Install fnm, a node version manager
 curl -fsSL https://fnm.vercel.app/install | bash -s -- --skip-shell
@@ -14,10 +12,4 @@ $fnm default lts-latest
 $fnm completions --shell bash >"$HOME/.bash_completion.d/_fnm"
 $fnm completions --shell zsh >"$HOME/.zfunc/_fnm"
 
-# Install pnpm, a node package manager
-$npm i -g pnpm
-
-# Install @angular/cli
-$npm install -g @angular/cli
-
-unset fnm_dir fnm npm
+unset fnm
