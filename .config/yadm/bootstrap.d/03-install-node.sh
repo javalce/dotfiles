54d9b431cecffca9db3813b8a4d651634e6f0b92
Javier Valero Cejudo <javalce29@gmail.com>
2023-07-27 12:56:59 +0200

    Fix usage of fnm and poetry binaries

--- .config/yadm/bootstrap.d/03-install-node.sh
+++ .config/yadm/bootstrap.d/03-install-node.sh
@@ -1,14 +1,16 @@
 #!/bin/sh
 
+fnm="$HOME/.local/share/fnm/fnm"
+
 # Install fnm, a node version manager
 curl -fsSL https://fnm.vercel.app/install | bash -s -- --skip-shell
 
-fnm install lts-latest
-fnm default lts-latest
+$fnm install lts-latest
+$fnm default lts-latest
 
 # Add fnm completions
-fnm completions --shell bash >"$HOME/.bash_completion.d/_fnm"
-fnm completions --shell zsh >"$HOME/.zfunc/_fnm"
+$fnm completions --shell bash >"$HOME/.bash_completion.d/_fnm"
+$fnm completions --shell zsh >"$HOME/.zfunc/_fnm"
 
 # Install pnpm, a node package manager
 npm i -g pnpm
