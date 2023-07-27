146cff70e368d408314689962b29cc07534b8f59
Javier Valero Cejudo <javalce29@gmail.com>
2023-07-27 14:43:28 +0200

    Fix npm command

--- .config/yadm/bootstrap.d/03-install-node.sh
+++ .config/yadm/bootstrap.d/03-install-node.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
-fnm="$HOME/.local/share/fnm/fnm"
+fnm_dir="$HOME/.local/share/fnm"
+fnm="$fnm_dir/fnm"
+npm="$fnm_dir/aliases/default/bin/npm"
 
 # Install fnm, a node version manager
 curl -fsSL https://fnm.vercel.app/install | bash -s -- --skip-shell
@@ -13,7 +15,9 @@ $fnm completions --shell bash >"$HOME/.bash_completion.d/_fnm"
 $fnm completions --shell zsh >"$HOME/.zfunc/_fnm"
 
 # Install pnpm, a node package manager
-npm i -g pnpm
+$npm i -g pnpm
 
 # Install @angular/cli
-npm install -g @angular/cli
+$npm install -g @angular/cli
+
+unset fnm_dir fnm npm
