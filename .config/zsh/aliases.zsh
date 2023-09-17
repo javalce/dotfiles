e99f76bfface3c1031f825b0acb0bbfacffd5974
Javier Valero Cejudo <javalce29@gmail.com>
2023-09-17 11:12:50 +0200

    Custom zsh config

---
+++ .config/zsh/aliases.zsh
@@ -0,0 +1,14 @@
+# some more ls aliases
+alias ll='ls -alh'
+alias la='ls -Alh'
+alias l='ls -CF'
+
+# add new aliases
+alias sshk='kitty +kitten ssh'
+alias icat='kitty +kitten icat'
+alias diffk='kitty +kitten diff'
+alias vim='nvim'
+# alias open='xdg-open'
+
+# yadm alias
+alias yadmsync="yadm add -u && yadm commit && yadm push"
