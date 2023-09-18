d6e6a455eda32786b354783cd05c5fd17122396e
Javier Valero Cejudo <javalce29@gmail.com>
2023-09-18 13:10:14 +0200

    Update zsh config

---
+++ .config/zsh/zsh_plugins.zsh
@@ -0,0 +1,18 @@
+fpath+=( /home/javalce/.cache/antidote/ohmyzsh/ohmyzsh )
+source /home/javalce/.cache/antidote/ohmyzsh/ohmyzsh/oh-my-zsh.sh
+fpath+=( /home/javalce/.cache/antidote/ohmyzsh/ohmyzsh/plugins/colored-man-pages )
+source /home/javalce/.cache/antidote/ohmyzsh/ohmyzsh/plugins/colored-man-pages/colored-man-pages.plugin.zsh
+fpath+=( /home/javalce/.cache/antidote/ohmyzsh/ohmyzsh/plugins/command-not-found )
+source /home/javalce/.cache/antidote/ohmyzsh/ohmyzsh/plugins/command-not-found/command-not-found.plugin.zsh
+fpath+=( /home/javalce/.cache/antidote/ohmyzsh/ohmyzsh/plugins/copybuffer )
+source /home/javalce/.cache/antidote/ohmyzsh/ohmyzsh/plugins/copybuffer/copybuffer.plugin.zsh
+fpath+=( /home/javalce/.cache/antidote/ohmyzsh/ohmyzsh/plugins/copyfile )
+source /home/javalce/.cache/antidote/ohmyzsh/ohmyzsh/plugins/copyfile/copyfile.plugin.zsh
+fpath+=( /home/javalce/.cache/antidote/ohmyzsh/ohmyzsh/plugins/git )
+source /home/javalce/.cache/antidote/ohmyzsh/ohmyzsh/plugins/git/git.plugin.zsh
+fpath+=( /home/javalce/.cache/antidote/ohmyzsh/ohmyzsh/plugins/git-flow-avh )
+source /home/javalce/.cache/antidote/ohmyzsh/ohmyzsh/plugins/git-flow-avh/git-flow-avh.plugin.zsh
+fpath+=( /home/javalce/.cache/antidote/zsh-users/zsh-syntax-highlighting )
+source /home/javalce/.cache/antidote/zsh-users/zsh-syntax-highlighting/zsh-syntax-highlighting.plugin.zsh
+fpath+=( /home/javalce/.cache/antidote/zsh-users/zsh-autosuggestions )
+source /home/javalce/.cache/antidote/zsh-users/zsh-autosuggestions/zsh-autosuggestions.plugin.zsh
