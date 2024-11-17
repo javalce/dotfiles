d0d42141942beeaea2c6b67c706484aa4ae7dba0
javalce <javalce29@gmail.com>
2024-11-17 17:13:35 +0100

    Update compress.sh script

--- bin/compress.sh
+++ bin/compress.sh
@@ -7,35 +7,46 @@ imagenes="Imágenes"
 videos="Vídeos"
 documentos="Documentos"
 dev="Dev"
+work="Work"
 
 # Go to home directory
 cd "$HOME"
 
 # Clean tmp directories
 tmp_files=(
-    'node_modules'
-    '.angular'
-    'dist'
-    '.next'
-    '.venv'
-    '.ruff_cache'
-    '.mypy_cache'
-    '.pytest_cache'
-    '__pycache__'
-    'htmlcov'
-    '.coverage'
+	'node_modules'
+	'.angular'
+	'dist'
+	'.next'
+	'.venv'
+	'.ruff_cache'
+	'.mypy_cache'
+	'.pytest_cache'
+	'__pycache__'
+	'htmlcov'
+	'.coverage'
 )
+
+clean_tmp_files() {
+	find "$1" -name "$tmp_file" -type d -prune -exec rm -rf '{}' \;
+}
+
+compress() {
+	tar -czvf "${1}.tgz" "$1"
+}
+
 for tmp_file in "${tmp_files[@]}"; do
-    find "$documentos" -name "$tmp_file" -type d -prune -exec rm -rf '{}' \;
-    find "$dev" -name "$tmp_file" -type d -prune -exec rm -rf '{}' \;
+	clean_tmp_files "$documentos"
+	clean_tmp_files "$dev"
+	clean_tmp_files "$work"
 done
 
-# Compress with tar
-tar -czvf "$descargas.tgz" "$descargas"
-tar -czvf "$imagenes.tgz" "$imagenes"
-tar -czvf "$videos.tgz" "$videos"
-tar -czvf "$documentos.tgz" "$documentos"
-tar -czvf "$dev.tgz" "$dev"
+compress "$descargas"
+compress "$imagenes"
+compress "$videos"
+compress "$documentos"
+compress "$dev"
+compress "$work"
 
 # Copy to external hard drive with rsync
 read -r -p 'Directory to copy files: ' DIRECTORY
