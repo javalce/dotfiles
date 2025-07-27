4a9f03d7e0303e7e48024c1b9d268cb3cf5bc096
Javier Valero Cejudo <javalce29@gmail.com>
2025-07-27 22:44:38 +0200

    chore: update nvim config

---
+++ .config/nvim/lua/plugins/vim-tmux-navigation.lua
@@ -0,0 +1,23 @@
+-- This file contains the configuration for the nvim-tmux-navigation plugin in Neovim.
+
+return {
+  -- Plugin: nvim-tmux-navigation
+  -- URL: https://github.com/alexghergh/nvim-tmux-navigation
+  -- Description: A Neovim plugin that allows seamless navigation between Neovim and tmux panes.
+  {
+    "alexghergh/nvim-tmux-navigation",
+
+    keys = function()
+      local nvim_tmux_navigation = require("nvim-tmux-navigation")
+
+      return {
+        { "<C-h>", nvim_tmux_navigation.NvimTmuxNavigateLeft, desc = "Navigate Left" },
+        { "<C-j>", nvim_tmux_navigation.NvimTmuxNavigateDown, desc = "Navigate Down" },
+        { "<C-k>", nvim_tmux_navigation.NvimTmuxNavigateUp, desc = "Navigate Up" },
+        { "<C-l>", nvim_tmux_navigation.NvimTmuxNavigateRight, desc = "Navigate Right" },
+        { "<C-\\>", nvim_tmux_navigation.NvimTmuxNavigateLastActive, desc = "Navigate Last Active" },
+        { "<C-Space>", nvim_tmux_navigation.NvimTmuxNavigateNext, desc = "Navigate Next" },
+      }
+    end,
+  },
+}
